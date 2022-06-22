a4de3bf5e8519035e9bd06150c30193bdb0f016a
MOAYAD <101659441+MOA-YAD@users.noreply.github.com>
2022-06-22 17:26:17 +0300

    Update dion_config.py

--- dion_config.py
+++ dion_config.py
@@ -6,8 +6,8 @@
 import os
 
 # Config Vars
-DIONAPI_HASH = "23c93aa64d16911f521bd0b16291af57"
-DIONAPI_KEY = 14624642
+DIONAPI_HASH = "4d3f4ff02426200734b5e39ad8ae1083"
+DIONAPI_KEY = 10480263
 DIONBOT_NAME = os.environ.get("BOT_NAME", None) # Your bot name, example: Dion Bot
 BOT_USERNAME = os.environ.get("BOT_USERNAME", None) # Your bot username with (@), example: @WhisperXRobot
 DION_TOKEN = os.environ.get("TOKEN", None) # Your token bot, get one from t.me/botfather
